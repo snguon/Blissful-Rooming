e82a9f327dde318e3a90d3c47918b8989a5cbb24
Connor Allan <connorallan2010@gmail.com>
2018-11-12 15:26:23 -0500

    Added footer with designed by element.

--- index.php
+++ index.php
@@ -6,4 +6,7 @@
     <meta name="keywords" content="HTML,CSS,PHP,JavaScript,MySQL">
     <meta name="author" content="Connor Allan, Samrety Nguon">
   </head>
+  <footer>
+    <p>Designed by: Connor Allan & Samrety Nguon</p>
+  </footer>
 </html>
