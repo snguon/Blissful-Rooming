b4c131217aaf7b71f109172cf05b6a3ffe7a1c1d
Connor Allan <connorallan2010@gmail.com>
2018-12-07 09:03:10 -0500

    Added calendar break down for events

--- calendar.php
+++ calendar.php
@@ -1,17 +1,31 @@
 <?php
 include 'lib/top.php';
 include 'lib/nav.php';
-?>
-<script type="text/javascript" src="js/calendar.js"></script>
-<body>
-  <div id="calendar-container">
-    <div id="calendar-header">
-      <span id="calendar-month-year"></span>
+$events = array(
+  array('DateTime' => '12/7/18 1:10 PM', 'Description' => 'CS Fair'),
+  array('DateTime' => '12/7/18 7:00 PM', 'Description' => 'Party'),
+  array('DateTime' => '12/8/18 12:00 AM', 'Description' => 'Naked Bike Ride'),
+  array('DateTime' => '12/9/18 8:00 PM', 'Description' => 'Wings and Football'));
+  ?>
+  <script type="text/javascript" src="js/calendar.js"></script>
+  <body>
+    <div id="calendar-container">
+      <div id="calendar-header">
+        <span id="calendar-month-year"></span>
+      </div>
+      <div id="calendar-dates">
+      </div>
     </div>
-    <div id="calendar-dates">
-    </div>
-  </div>
-</body>
-<?php
-include 'lib/footer.php';
-?>
+    <article>
+      <table><tr><thead><th>Description</th><th>Date</th></thead></tr><tbody>
+        <?php
+        for($i = 0; $i <= 3; $i++){
+          print '<tr><td>' . $events[$i]['Description'] . '</td><td>' . $events[$i]['DateTime'] . '</td></tr>';
+        }
+        ?>
+      </tbody></table>
+    </article>
+  </body>
+  <?php
+  include 'lib/footer.php';
+  ?>
