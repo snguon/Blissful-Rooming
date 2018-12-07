3d5aa85cb7514e54143c96993d2791657cac89c9
Sam Nguon <samrety.nguon@uvm.edu>
2018-12-07 03:18:26 -0500

    Ground work for Chores and IOUs pages

---
+++ ious.php
@@ -0,0 +1,20 @@
+<?php
+include 'lib/top.php';
+include 'lib/nav.php';
+$ious = array(array('Requested' => 'Connor','Payer' => 'Sam','Reason' => 'Coffee', 'Amount' => 20),array('Requested' => 'Sam','Payer' => 'Aaron','Reason' => 'Food', 'Amount' => 30),array('Requested' => 'Aaron','Payer' => 'Connor','Reason' => 'Booze', 'Amount' => 40));
+?>
+<article id="iousList">
+  <div class="ious">
+    <?php
+    print '<h3>IOUs List</h3><table>';
+    print '<tr><th>Requested By</th><th>Payer</th><th>Reason</th><th>Amount</th></tr>';
+    for ($i = 0; $i <= 2; $i++) {
+      print '<tr><td>'.$ious[$i]['Requested'].'</td><td>$ '.$ious[$i]['Payer'].'</td><td>'.$ious[$i]['Reason'].'</td><td>$ '.$ious[$i]['Amount'].'</td></tr>';
+    }
+    print '</table>';
+    ?>
+  </div>
+</article>
+<?php
+include 'lib/footer.php';
+?>
